be82ebf5c6fd9a29b66918c8862663089a173c16
sam43 <scode43@gmail.com>
2016-08-10 13:56:55 +0600

    Custom Navigation Bar with Sub menus
    Custom Navigation Bar with Sub menus for Ecommerce website. Customable
    usage

---
+++ app/src/main/java/example/saadat/com/customnavbar/HomeFragment.java
@@ -0,0 +1,32 @@
+package example.saadat.com.customnavbar;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by Saadat on 8/9/2016.
+ */
+public class HomeFragment extends Fragment {
+
+    /*
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }*/
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home,
+                container, false);
+
+        return view;
+    }
+}
+
+
